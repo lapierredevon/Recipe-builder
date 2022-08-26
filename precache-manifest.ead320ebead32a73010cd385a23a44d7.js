48eb67b1dba80f870a6e2ba2a2dc26bde72e4c47
Devon Lapierre <lapierredevon@gmail.com>
2022-08-26 19:26:31 -0400

    Updates

---
+++ precache-manifest.ead320ebead32a73010cd385a23a44d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0d0d862a788458e2097a73ead19c8c1",
+    "url": "/Recipe-builder/index.html"
+  },
+  {
+    "revision": "f82909355712f6d64d67",
+    "url": "/Recipe-builder/static/css/main.7b1d3749.chunk.css"
+  },
+  {
+    "revision": "d805d34cf633678ad908",
+    "url": "/Recipe-builder/static/js/2.3fde9c21.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Recipe-builder/static/js/2.3fde9c21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f82909355712f6d64d67",
+    "url": "/Recipe-builder/static/js/main.d8446457.chunk.js"
+  },
+  {
+    "revision": "6d6e6c48b9fefc6c5c27",
+    "url": "/Recipe-builder/static/js/runtime-main.e97c2e10.js"
+  }
+]);
